48f02423c2f311450cabaa02118f7767070700e3
jean-ka78 <jean@ukr.net>
2020-11-13 23:38:41 +0200

    relase

--- json.h
+++ json.h
@@ -41,19 +41,19 @@ DynamicJsonDocument doc_post(capacity_post);
 JsonArray tags = doc_post.createNestedArray("tags");
 JsonObject tags_0 = tags.createNestedObject();
 tags_0["id"] = 1;
-tags_0["value"] = TempZ[0];
+tags_0["value"] = Temp_in;
 
 JsonObject tags_1 = tags.createNestedObject();
 tags_1["id"] = 2;
-tags_1["value"] = TempZ[1];
+tags_1["value"] = Temp_out;
 
 JsonObject tags_2 = tags.createNestedObject();
 tags_2["id"] = 3;
-tags_2["value"] = TempZ[2];
+tags_2["value"] = TempZ[0];
 
 JsonObject tags_3 = tags.createNestedObject();
 tags_3["id"] = 4;
-tags_3["value"] = TempZ[3];
+tags_3["value"] = TempZ[2];
 
 JsonObject tags_4 = tags.createNestedObject();
 tags_4["id"] = 5;
